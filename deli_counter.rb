ce6b69d8d71e81970e535ea189d2eccf2bfc2e73
Janice Pena <jipena94@gmail.com>
2019-04-14 01:08:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli[]
+  puts "The line is currently empty."
+else
+  puts 
+end
+
+name = "Ada"
+
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number 1 in line." 
+end
